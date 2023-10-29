99f63734d7075d7c64a1cbda6254c0e5eae22765
AlistairFR <jbwinckel62370@gmail.com>
2023-10-29 22:13:02 +0100

    FIX login redirect by redirect outside try/catch

--- auth.ts
+++ auth.ts
@@ -1,19 +1,19 @@
 import NextAuth from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
-import { authConfig } from './auth.config';
+import bcrypt from 'bcrypt';
 import { sql } from '@vercel/postgres';
 import { z } from 'zod';
 import type { User } from '@/app/lib/definitions';
-import bcrypt from 'bcrypt';
+import { authConfig } from './auth.config';
 
 async function getUser(email: string): Promise<User | undefined> {
-    try {
-      const user = await sql<User>`SELECT * from USERS where email=${email}`;
-      return user.rows[0];
-    } catch (error) {
-      console.error('Failed to fetch user:', error);
-      throw new Error('Failed to fetch user.');
-    }
+  try {
+    const user = await sql<User>`SELECT * from USERS where email=${email}`;
+    return user.rows[0];
+  } catch (error) {
+    console.error('Failed to fetch user:', error);
+    throw new Error('Failed to fetch user.');
+  }
 }
 
 export const { auth, signIn, signOut } = NextAuth({
@@ -26,22 +26,21 @@ export const { auth, signIn, signOut } = NextAuth({
           .safeParse(credentials);
 
         if (parsedCredentials.success) {
-            const { email, password } = parsedCredentials.data;
-            const user = await getUser(email);
-            //Test
-            console.log("user", user);
+          const { email, password } = parsedCredentials.data;
 
-            if (!user) return null;
-            const passwordsMatch = await bcrypt.compare(password, user.password);
-            //Test
-            console.log("passwordsMatch", passwordsMatch);
+          const user = await getUser(email);
+          if (!user) return null;
 
-            if (passwordsMatch) return user;
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+          if (passwordsMatch) {
+            console.log('Successfully authenticated user', user);
+            return user;
+          }
         }
 
         console.log('Invalid credentials');
         return null;
-      }
-    })
-  ]
+      },
+    }),
+  ],
 });
